4ead199923055f6fa3bdbb1235949e030af54228
artem <iakonien@gmail.com>
2024-12-20 16:42:17 +0300

    ...

--- src/main/java/org/artemyakkonen/spring/rest/App.java
+++ src/main/java/org/artemyakkonen/spring/rest/App.java
@@ -1,13 +1,22 @@
 package org.artemyakkonen.spring.rest;
 
+import org.artemyakkonen.spring.rest.configuration.MyConfig;
+import org.artemyakkonen.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
     }
 }
